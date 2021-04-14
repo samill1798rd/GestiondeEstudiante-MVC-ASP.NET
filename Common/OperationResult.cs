3820d08c5aa5470bfe4a3f5bff9b5efe9159c54d
samill1798rd <63328089+samill1798rd@users.noreply.github.com>
2021-04-13 22:45:18 -0400

    Creacion de Arquitectura

---
+++ Common/OperationResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class OperationResult<T>
+    {
+        public OperationResult()
+        {
+            Mensaje = new List<string>();
+        }
+
+        public bool Ok { get; set; }
+        public T ResultObject { get; set; }
+        public List<string> Mensaje { get; set; }
+    }
+}
---
+++ Web/App_Start/UnityConfig.cs
@@ -0,0 +1,22 @@
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+
+namespace Web
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+            
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            
+            // e.g. container.RegisterType<ITestService, TestService>();
+            
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+            //container.RegisterType<ITestService, TestService>();
+        }
+    }
+}
