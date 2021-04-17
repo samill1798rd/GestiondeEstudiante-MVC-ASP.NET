d5be4e480b322fa41424f3610253ebe93ea100be
CristianVargas03 <65677401+CristianVargas03@users.noreply.github.com>
2021-04-17 13:58:09 -0400

    Subiendo LoginCRUD
    Co-Authored-By: eliotvillarc <64939423+eliotvillarc@users.noreply.github.com>

---
+++ LoginCRUD/LoginCRUD/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginCRUD.Models;
+using LoginCRUD.Controllers;
+using System.Data.SqlClient;
+
+namespace LoginCRUD.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void ConnetionString()
+        {
+            con.ConnectionString = @"data source=LAPTOP-CV-2O729\SQLEXPRESS; database=GestionBD; integrated security = SSPI;";
+        }
+
+        [HttpPost]
+        public ActionResult Verificar(Account user, string mensaje)
+        {
+            
+            ConnetionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = $"select * from Usuario where NombreUsuario ='{user.NombreUsuario}' and Clave='{user.Clave}'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return RedirectToAction("Index","Estudiantes");
+            }
+            else
+            {
+                con.Close();
+                return View();
+            }
+
+        }
+
+        public ActionResult SingUp(string mensaje = "")
+        {
+            ViewBag.Mensaje = mensaje;
+            return View("Singup");
+        }
+
+        [HttpPost]
+        public ActionResult Registrer(Account ac)
+        {
+            ac.FechaCreacion = DateTime.Now.ToString("yyyy-MM-dd");
+            ConnetionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "INSERT INTO Usuario values ( '" + ac.NombreUsuario + "', '" + ac.Clave + "', '" + ac.FechaCreacion + "', 1)";
+            com.ExecuteNonQuery();
+            con.Close();
+            return SingUp("Usuario Creado!");
+
+        }
+    }
+}
