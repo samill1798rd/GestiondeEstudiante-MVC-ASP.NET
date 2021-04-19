b77caa4adfd520a860bad02e3e20bb71980350ac
Cristopher Severino <cristopherseverino82@gmail.com>
2021-04-19 00:05:57 -0400

    prueba de solucion de errores

--- Services/EstudianteServices/IEstudianteServices.cs
+++ Services/EstudianteServices/IEstudianteServices.cs
@@ -1,10 +1,6 @@
 ﻿using Common;
 using DataAccess;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace Services.EstudianteServices
--- Web/ViewModel/EstudianteViewModel.cs
+++ Web/ViewModel/EstudianteViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using DataAccess;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace Web.ViewModel
 {
